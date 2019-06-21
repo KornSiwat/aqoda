63fe7169808b6a577b2e202b46634d7465cb3821
KornSiwat <ponpued@gmail.com>
2019-06-22 00:12:03 +0700

    Initial Commit

---
+++ aqoda/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  aqoda
+//
+//  Created by Siwat Ponpued on 6/22/19.
+//  Copyright © 2019 Siwat Ponpued. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
